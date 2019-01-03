77e57f6a56e93872ef2aeaa91c1aa5b2491cb200
Wilson Wu <wilsoncwu@gmail.com>
2019-01-02 20:00:07 -0500

    uses arduino

--- Servo.py
+++ Servo.py
@@ -1,35 +1,20 @@
 # Servo Control
 import time
-import wiringpi
- 
-# use 'GPIO naming'
-wiringpi.wiringPiSetupGpio()
- 
-# set #18 to be a PWM output
-wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
- 
-# set the PWM mode to milliseconds stype
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
- 
-# divide down clock
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
- 
-delay_period = 3
-hinge_closed = 200
-hinge_opened = 130
-wedge_closed = 180
-wedge_opened = 90
-#wiringpi.pwmWrite(18, closed_position)
+import smbus
+
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+#Slave Address 1
+address = 0x04
 
 print("Ran init for servo")
 
 def OpenDoor():
   print("Opening door")
-  #wiringpi.pwmWrite(18, hinge_opened)
-  #time.sleep(delay_period)
+  bus.write_byte(address, 1)
 
 def CloseDoor():
   print("Closing door")
-  #wiringpi.pwmWrite(18, hinge_closed)
-  #time.sleep(delay_period)
+  bus.write_byte(address, 0)
